e50dd182d56ea3e9eca3792abdb09b9dc5f84d58
MollyMai99 <miaoling.mai99@gmail.com>
2024-04-01 13:41:52 +0800

    4.1update

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 /*----- constants -----*/
-const picture = { 1: "", 2: "" };
+const picture = [
+  "https://picsum.photos/id/8/5000/3333",
+  "https://picsum.photos/id/49/1280/792",
+];
 const WINNING_COMBOS = [];
+const gameStatus = ["Start", "RightContinue", "WrongContinue", "End"];
+/* 
+Game Status:
+Start: choose two pictures;
+RightContinue: these two pictures are matched;
+WrongContinue: these two pictures are not matched;
+End: you match all pictures, win the game 
+*/
+
+console.log(picture);
 
 /*----- state variables -----*/
 let board;
@@ -18,12 +31,20 @@ initialize();
 
 function initialize() {
   console.log("initialize");
+  board = [picture[1], picture[2], picture[1], picture[2]];
+  gameStatu = gameStatus[0];
+  render();
 }
 
 function handleShow(evt) {
   console.log(evt);
   console.log("handleShow");
+  checkGameResult;
   render();
+}
+
+function checkGameResult() {
+  console.log("checkgameresult");
 }
 
 function render() {
@@ -37,4 +58,13 @@ function renderBoard() {
 
 function renderMessage() {
   console.log("renderMessage");
+  if (gameStatu === gameStatus[0]) {
+    console.log("game status is start");
+  } else if (gameStatu === gameStatus[1]) {
+    console.log("game status is right continue");
+  } else if (gameStatu === gameStatus[2]) {
+    console.log("game status is wrong continue");
+  } else if (gameStatu === gameStatus[3]) {
+    console.log("game status is end");
+  }
 }
